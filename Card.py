bd46b49f6753c225b176d411c57ec93d24692f6a
伊藤千容 <abc.126.a5k@gmail.com>
2020-11-15 02:46:00 +0900

    Card.py タイプミスの修正

--- Card.py
+++ Card.py
@@ -1,7 +1,7 @@
 class Card:
 	suits = ["spades", "hearts", "diamonds", "clubs"]
 
-	values = [None, None, "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" "Ace"]
+	values = [None, None, "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
 
 	def __init__(self, v, s):
 		"""スート(マーク)も値も整数値です"""
